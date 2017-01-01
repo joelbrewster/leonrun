8ab62cbe7d8ed38427be9218f2c7623bcbc4800a
Joel Brewster <hi@joelbrewster.com>
2017-01-01 16:03:37 +1100

    Generate boxes

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ var Game = {
 Game.map = {};
 Game._generateMap = function() {
   var digger = new ROT.Map.Digger();
+  var freeCells = [];
 
   var digCallback = function(x, y, value) {
     if (value) {
@@ -15,9 +16,22 @@ Game._generateMap = function() {
     } /* Do not store walls */
 
     var key = x+","+y;
+    freeCells.push(key);
     this.map[key] = ".";
   };
   digger.create(digCallback.bind(this));
+
+  this._generateBoxes(freeCells);
+
+  this._drawWholeMap();
+};
+
+Game._generateBoxes = function(freeCells) {
+  for (var i=0; i<10; i++) {
+    var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+    var key = freeCells.splice(index, 1)[0];
+    this.map[key] = "*";
+  }
 };
 
 Game._drawWholeMap = function() {
