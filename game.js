b37b9410d69375a021da0c1c00125cc242fb42f7
Joel Brewster <hi@joelbrewster.com>
2017-01-01 15:44:39 +1100

    Generating a dungeon map

--- game.js
+++ game.js
@@ -4,3 +4,16 @@ var Game = {
     document.body.appendChild(this.display.getContainer());
   }
 };
+
+Game.map = {};
+Game._generateMap = function() {
+  var digger = new ROT.Map.Digger();
+
+  var digCallback = function(x, y, value) {
+    if (value) { return; } /* Do not store walls */
+
+   var key = x+","+y;
+   this.map[key] = ".";
+ };
+ digger.create(digCallback.bind(this));
+};
